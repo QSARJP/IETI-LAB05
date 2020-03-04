7161106839ab86bc91380bf4b03bc42264268d2f
Juan Ospina <juan.ospina-h@mail.escuelaing.edu.co>
2020-03-04 13:40:00 -0500

    manifest

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -30,13 +30,32 @@ export function register(config) {
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
     }
-    
-         
 
-     //Add this part of code to your serviceWorker.js
-     window.addEventListener('install', () => {
+    //Add this part of code to your serviceWorker.js
+    window.addEventListener('install', () => {
       console.log("install!!");
-  });
+    });
+
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (isLocalhost) {
+        // This is running on localhost. Let's check if a service worker still exists or not.
+        checkValidServiceWorker(swUrl, config);
+
+        // Add some additional logging to localhost, pointing developers to the
+        // service worker/PWA documentation.
+        navigator.serviceWorker.ready.then(() => {
+          console.log(
+            'This web app is being served cache-first by a service ' +
+              'worker. To learn more, visit https://bit.ly/CRA-PWA'
+          );
+        });
+      } else {
+        // Is not localhost. Just register service worker
+        registerValidSW(swUrl, config);
+      }
+    });
   }
 }
 
